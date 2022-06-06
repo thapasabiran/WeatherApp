056bae273ce5c15cb286bfcd47f54db8a053feb4
Oleg Klyushnik <medvezhatneg@gmail.com>
2022-06-06 08:43:03 -0700

    updated Retrofit to use String instead of Weather, also switched to onecall call instead of weather call

--- app/src/main/java/com/example/weatherapp/database/JsonDbHelper.kt
+++ app/src/main/java/com/example/weatherapp/database/JsonDbHelper.kt
@@ -1,5 +1,6 @@
 package com.example.weatherapp.database
 
+import android.content.Context
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -14,5 +15,20 @@ class JsonDbHelper {
         CoroutineScope(Dispatchers.IO).launch{
 
         }
+    }
+
+    fun getJsonFromAssets(fileName: String, context: Context): String? {
+        /*
+        open file - stream of bytes
+        buffer - a byte array, read bytes from the file
+         */
+        var jsonString: String? = null
+        val inStream = context.assets.open("MockData.json")
+        val size = inStream.available()
+        val buffer = ByteArray(size)
+        inStream.read(buffer)
+        inStream.close()
+        jsonString = String(buffer, Charsets.UTF_8)
+        return jsonString
     }
 }
