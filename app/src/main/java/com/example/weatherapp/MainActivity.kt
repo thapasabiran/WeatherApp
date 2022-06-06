55456e36326bdee70503047328c9fe3bd68b69bd
Sabiran Thapa <sabiran.thapa@crestline.net>
2022-06-06 17:09:46 -0400

    Added FrontPageActivity and modified main activity layout

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.weatherapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.weatherapp.api.RetroApiInterface
 import com.example.weatherapp.api.WeatherRepository
 import com.example.weatherapp.api.WeatherViewModel
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     lateinit var vm : WeatherViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
-        val api = RetroApiInterface.create()
-        val repo = WeatherRepository(api)
-        vm = WeatherViewModel(repo)
-        vm.weather.observe(this) {
-            println(it)
+        binding.welcomeContinueButton.setOnClickListener {
+            val frontPageIntent = Intent(this, FrontPageActivity::class.java)
+            startActivity(frontPageIntent)
         }
-        vm.getWeather("35","139")
+
+//        val api = RetroApiInterface.create()
+//        val repo = WeatherRepository(api)
+//        vm = WeatherViewModel(repo)
+//        vm.weather.observe(this) {
+//            println(it)
+//        }
+//        vm.getWeather("35","139")
     }
 }
