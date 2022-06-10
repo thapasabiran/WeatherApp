e8a8e6ad9c73b077e4c7fe3991c949a96a9bb52c
Sabiran Thapa <sabiran.thapa@crestline.net>
2022-06-10 16:34:09 -0400

    Refactored code and added custom loading dialog

--- app/src/main/java/com/example/weatherapp/ForecastActivity.kt
+++ app/src/main/java/com/example/weatherapp/ForecastActivity.kt
@@ -1,10 +1,10 @@
 package com.example.weatherapp
 
-import android.content.Context
 import android.os.Bundle
-import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.widget.AbsListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherapp.adapters.DailyWeatherAdapter
 import com.example.weatherapp.adapters.HourlyWeatherAdapter
 import com.example.weatherapp.api.RetroApiInterface
@@ -14,7 +14,6 @@ import com.example.weatherapp.database.DailyWeather
 import com.example.weatherapp.database.HourlyWeather
 import com.example.weatherapp.databinding.ActivityForecastBinding
 
-
 class ForecastActivity : AppCompatActivity() {
     lateinit var binding: ActivityForecastBinding
     lateinit var dailyWeatherList: ArrayList<DailyWeather>
@@ -23,12 +22,16 @@ class ForecastActivity : AppCompatActivity() {
     lateinit var hourlyWeatherAdapter: HourlyWeatherAdapter
     lateinit var loadingDialog: LoadingDialog
     lateinit var vm : WeatherViewModel
+    var isScrolling = false
+    var curentItems = 0
+    var totalItems = 0
+    var scrollOutItems = 0
+    var offSet = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForecastBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        loadingDialog = LoadingDialog(this)
 
         binding.forecastWetConTextView.text = intent.getStringExtra("weatherCondition")
         binding.forecastTemTextView.text = intent.getStringExtra("temp")
@@ -36,49 +39,70 @@ class ForecastActivity : AppCompatActivity() {
 
         val api = RetroApiInterface.create()
         val repo = WeatherRepository(api, this)
+        loadingDialog = LoadingDialog(this)
 
         vm = WeatherViewModel(repo)
 
         dailyWeatherList = ArrayList<DailyWeather>()
-        loadingDialog.startLoadingDialog()
-        vm.getDailyWeather()?.observe(this) {
-            dailyWeatherList = it as ArrayList<DailyWeather> /* = java.util.ArrayList<com.example.weatherapp.database.DailyWeather> */
-            dailyWeatherAdapter.setDailyWeather(dailyWeatherList)
-        }
-
-        hourlyWeatherList = ArrayList<HourlyWeather>()
-        vm.getHourlyWeather()?.observe(this) {
-            hourlyWeatherList = it as ArrayList<HourlyWeather> /* = java.util.ArrayList<com.example.weatherapp.database.DailyWeather> */
-            hourlyWeatherAdapter.setHourlyWeather(hourlyWeatherList)
-//            loadingDialog.dismissDialog()
-        }
-
-        val pref = getSharedPreferences("prefs", Context.MODE_PRIVATE)
         var dailyRecyclerView = binding.dailyRecyclerView
-        dailyWeatherAdapter = DailyWeatherAdapter(dailyWeatherList, pref)
+        dailyWeatherAdapter = DailyWeatherAdapter(dailyWeatherList)
         dailyRecyclerView.layoutManager = LinearLayoutManager(this)
         dailyRecyclerView.adapter = dailyWeatherAdapter
 
+        //start loading dialog
+        loadingDialog.startLoadingDialog()
+        //get the daily weather and bind it to the recycler view adapter
+        vm.getDailyWeather()?.observe(this) {
+            dailyWeatherList = it as ArrayList<DailyWeather> /* = java.util.ArrayList<com.example.weatherapp.database.DailyWeather> */
+            //To get min and max temp of today weather we are using the first element in list that contains today weather data
+            binding.forecastLowTextView.text = dailyWeatherList[0].temp_min.toString()
+            binding.forecastHighTextView.text = dailyWeatherList[0].temp_max.toString()
+            dailyWeatherAdapter.setDailyWeather(dailyWeatherList)
+        }
+
+        //Get the first 12 hourly weather report and after that only load more if user start scrolling for more data
+        hourlyWeatherList = ArrayList<HourlyWeather>()
+        vm.getHourlyWeather(12, offSet)?.observe(this) {
+            hourlyWeatherList = it as ArrayList<HourlyWeather> /* = java.util.ArrayList<com.example.weatherapp.database.DailyWeather> */
+            hourlyWeatherAdapter.setHourlyWeather(hourlyWeatherList)
+        }
 
         var hourlyRecyclerView = binding.hourlyRecyclerView
-        hourlyWeatherAdapter = HourlyWeatherAdapter(hourlyWeatherList, pref)
-        hourlyRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true)
+        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        hourlyWeatherAdapter = HourlyWeatherAdapter(hourlyWeatherList)
+        hourlyRecyclerView.layoutManager = layoutManager
         hourlyRecyclerView.adapter = hourlyWeatherAdapter
-        hourlyRecyclerView
-            .getViewTreeObserver()
-            .addOnGlobalLayoutListener(
-                object : OnGlobalLayoutListener {
-                    override fun onGlobalLayout() {
-                        // At this point the layout is complete and the
-                        // dimensions of recyclerView and any child views
-                        // are known.
 
-                        hourlyRecyclerView
-                            .getViewTreeObserver()
-                            .removeOnGlobalLayoutListener(this)
+        //if user start scrolling for more data, get it from db based on offset
+        hourlyRecyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+                    isScrolling = true
+            }
 
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                curentItems = layoutManager.childCount
+                totalItems = layoutManager.itemCount
+                scrollOutItems = layoutManager.findFirstVisibleItemPosition()
+                //dismiss loading dialog
+                loadingDialog.dismissDialog()
+
+                //Size of hourForecast table is 48, we can also get size and use it
+                if (isScrolling && ((curentItems + scrollOutItems) == totalItems) && offSet < 48) {
+                    isScrolling = false
+                    offSet += 12 //increment offset to get 12 more data once we reach end of current 12 data
+                    //start loading dialog until extra 12 data get fetched
+                    loadingDialog.startLoadingDialog()
+                    vm.getHourlyWeather(12, offSet)?.observe(this@ForecastActivity) {
+                        hourlyWeatherList.addAll(it as ArrayList<HourlyWeather>) /* = java.util.ArrayList<com.example.weatherapp.database.DailyWeather> */
+                        hourlyWeatherAdapter.setHourlyWeather(hourlyWeatherList)
+                        //dismiss loading dialog
                         loadingDialog.dismissDialog()
                     }
-                })
+                }
+            }
+        })
     }
 }
